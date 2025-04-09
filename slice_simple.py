e8447c9a8d627654e481e5b487ecaf6756958961
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-04-09 17:17:19 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -2,6 +2,17 @@ def slice_simple():
     texto = "Awesome"
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+    #31/3/2025
+    #TP3 - Rozas Miguel Agustin - Introduccion a la programacion 1
+    # Ejercicio 2
+    first_three = texto[0:3]
+    middle_text = int(len(texto)/2)
+    middle_three = texto[middle_text-1:middle_text+2]
+    first_four = texto[0:4]
+    last_three = texto[-3:]
+    print(first_three.lower())
+    print(middle_three.lower())
+    print(first_four.lower() + last_three.lower())
 
 
 # Para verificar este ejercicio ejecutar el comando
